72bbc2390e184c7efbe48d08ce3a45e02b870e28
Ann <4nncancode@gmail.com>
2019-07-07 14:48:47 -0400

    encryption to hash instead

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
+// const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -21,10 +22,10 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt, {
-    secret: process.env.API, 
-    encryptedFields: ["password"]
-});
+// userSchema.plugin(encrypt, {
+//     secret: process.env.API, 
+//     encryptedFields: ["password"]
+// });
 
 const User = new mongoose.model("User", userSchema); 
 
@@ -43,7 +44,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if (err) {
